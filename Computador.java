0e7679b19c4162ca348eddb8d7f540121627934e
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-06-06 10:38:40 -0300

    Create Computador.java

---
+++ Computador.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Computador {
+
+	private String processador;
+	
+	private String memoria;
+	
+	private Float preco;
+
+	//GETS E SETS
+	
+	public String getProcessador() {
+		return processador;
+	}
+
+	public void setProcessador(String processador) {
+		this.processador = processador;
+	}
+
+	public String getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
